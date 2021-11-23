3e8dee4d808a22ce1296867dbf8d231a6a999593
ConnorP1 <prebc191@newschool.edu>
2021-11-23 04:25:37 -0500

    Final Bosses

---
+++ Exercises/JS/P5.JS/main3.js
@@ -0,0 +1,30 @@
+function setup() {}
+
+function draw() {
+  triangle(30, 75, 58, 20, 86, 75);
+}
+
+function setup() {}
+
+function draw() {
+  triangle(30, 75, 58, 20, 86, 75);
+  rect(40, 40, 8, 8);
+}
+
+function setup() {}
+
+function draw() {
+  background("seagreen");
+  triangle(30, 75, 58, 20, 86, 75);
+  rect(40, 40, 8, 8);
+}
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("seagreen");
+  triangle(30, 75, 58, 20, 86, 75);
+  rect(40, 40, 8, 8);
+}
